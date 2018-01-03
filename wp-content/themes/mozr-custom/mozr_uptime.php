e0089f6408f836252b8895104e66e45ad26dd5b4
Riquelmy Melara <melarr01@stepstone.com>
2018-01-03 09:50:37 -0600

    Mozr Uptime Page Template

---
+++ wp-content/themes/mozr-custom/mozr_uptime.php
@@ -0,0 +1,43 @@
+<?php
+ /*
+ Template Name: MOZR Uptime
+ */
+ get_header(); ?>
+    </div><!-- Content  -->
+    <img src="<?php echo get_stylesheet_directory_uri() ?>/img/uptime_image.jpg"  alt="">
+
+</div> <!-- Row  -->
+<div class=""> <!-- row -->
+   <div id="content" class="container">
+
+        <div class="col-xs-12 col-md-8 col-md-offset-2 post-cont">
+            <article class="uptime-container">
+            <p>Mozr leverages the power of Amazon Web Services (AWS), which allows us to provide a</p>
+
+            <p>99.95% uptime guarantee. We prioritize our customers’ speed, stability, scalability and sus-
+            tainability. Our guarantee ensures that your applications and websites will always stay availa-
+            ble for your customers regardless of your location, business hours, and peak traffic times.</p>
+
+            <p>Every reasonable effort is used to keep your data safe, ensure sustainability for your applica-
+            tions and consistently maintain availability for customers 24/7. Without a good host that fo-
+            cuses on uptime, you could lose millions in revenue from downtime.</p>
+
+            <p>Having good uptime affects brand loyalty. Imagine visiting a site frequently down due to tech-
+            nical issues. Would you continue to visit the site and provide the business with sensitive data?</p>
+
+            <p>Or would you find an alternative? Statistics show that most customers leave a site after
+            three-second delays in load time. Your business revenue depends on uptime, and our service
+            provides only the best in infrastructure. Our leveraged AWS infrastructure improves brand
+            loyalty and avoids lost revenue due to technical issue.</p>
+
+            <p>Not only does Mozr provide superior uptime guarantees, but our AWS hosting also contains
+            monitoring services in case of an event. Amazon monitoring provides a way for Mozr and you
+            to identify an event before it becomes a disaster. It gives us and AWS IT the ability to remedy
+            the situation before downtime occurs.</p>
+
+            </article>
+        </div>
+
+
+<?php
+ get_footer();
