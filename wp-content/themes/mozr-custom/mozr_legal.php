534719e24430b5b781d0fc94137468d8ac4ad5b4
Riquelmy Melara <riquelmy.melara@tecniastudio.com>
2018-01-14 16:23:49 -0600

    Updating Legal Template

---
+++ wp-content/themes/mozr-custom/mozr_legal.php
@@ -0,0 +1,53 @@
+<?php
+ /*
+ Template Name: MOZR Legal
+ */
+ get_header(); ?>
+</div><!-- Content  -->
+    
+    <div class="container jb-mobile legal-container" style="background-image:url(<?php echo get_stylesheet_directory_uri() ?>/img/legal-hero-image.jpg)">
+    
+        <div class="col-md-6 mozr-page-title legal">
+            <h1>
+            Service Level Agreement
+            </h1>
+            
+        </div>
+   
+    </div>
+</div>
+
+<div class="col-xs-12 post-cont">
+
+    <div class="container margin-bottom-50">
+        <div class="col-sm-4">
+        <h4 class="mozr-grey-bk legal-button">Terms of Service</h4>
+        <h4 class="mozr-grey-bk legal-button legal-active">Service Level Agreement</h4>
+        <h4 class="mozr-grey-bk legal-button">Privacy and Copyright</h4>
+        <h4 class="mozr-grey-bk legal-button">Acceptable Use Policy</h4>
+        </div>
+        
+        <div class="col-sm-8">
+            <h2>Service Level Agreement Title</h2>
+            <p>Since Mozr works exclusively with AWS for its speed and scalability, plugging Route 53 DNS
+
+            services into your applications is simple configurations and integration. The service is designed 
+            to work well and easily integrate into other Amazon offerings.</p>
+            <p>Our leveraged DNS services are designed to be fast and simple. Whether you’re a developer
+            that needs easily managed DNS or a business owner with little knowledge of the way the DNS
+            protocol works, Mozr is here to help. Route 53 has low latency and even a self-service API for
+            development workflows.</p>
+            <p>Route 53 gives you hosted zone capabilities providing flexibility to add DNS records at-will, or
+            site owners can transfer DNS hosting services to Amazon for a currently hosted domain. Auto-
+            matically integrate load balancers and route customers to an Amazon EC2 region.</p>
+            <p>Whatever your DNS needs, Mozr can host and set up a fully provisioned DNS service configured 
+                for your unique business purposes.</p>
+        </div>
+    </div>
+
+</div>
+
+<div class=""> <!-- row -->
+   <div id="content" class="container">
+<?php
+ get_footer();
