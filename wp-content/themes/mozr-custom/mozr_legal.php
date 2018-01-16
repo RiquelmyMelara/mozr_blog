30049e8de42886bda4a694f143e8e040d1ff4fb1
Riquelmy Melara <melarr01@stepstone.com>
2018-01-16 17:27:43 -0600

    - Service Level Agreement

--- wp-content/themes/mozr-custom/mozr_legal.php
+++ wp-content/themes/mozr-custom/mozr_legal.php
@@ -27,21 +27,32 @@
         <h4 linkto="acceptable-use-policy" class="mozr-grey-bk legal-button footer-link">Acceptable Use Policy</h4>
         </div>
         
-        <div class="col-sm-8">
-            <h2>Service Level Agreement Title</h2>
-            <p>Since Mozr works exclusively with AWS for its speed and scalability, plugging Route 53 DNS
-
-            services into your applications is simple configurations and integration. The service is designed 
-            to work well and easily integrate into other Amazon offerings.</p>
-            <p>Our leveraged DNS services are designed to be fast and simple. Whether you’re a developer
-            that needs easily managed DNS or a business owner with little knowledge of the way the DNS
-            protocol works, Mozr is here to help. Route 53 has low latency and even a self-service API for
-            development workflows.</p>
-            <p>Route 53 gives you hosted zone capabilities providing flexibility to add DNS records at-will, or
-            site owners can transfer DNS hosting services to Amazon for a currently hosted domain. Auto-
-            matically integrate load balancers and route customers to an Amazon EC2 region.</p>
-            <p>Whatever your DNS needs, Mozr can host and set up a fully provisioned DNS service configured 
-                for your unique business purposes.</p>
+        <div class="col-sm-8 legal-font-size">
+            <p>MOZR provides an industry-leading product, and is pleased to support it with this Service Level Agreement (“<b>SLA</b>”). 
+                This SLA is incorporated into Customer’s Agreement with us, and made a part of it. Terms not defined in this SLA have the definitions set out in the Agreement. The remedies set out in this SLA are Customer’s sole and exclusive remedy for issues covered by the SLA. While we will not modify this SLA arbitrarily, we may do so from time-to-time. Should we make a change to this SLA, we shall notify Customer (e.g. by email or by notification in the Customer control panel). 
+                The notification will set out the effective date of any changes. It is important that Customer review the SLA completely.</p>
+            <h2>1. SERVICE AVAILABILITY.</h2>
+            <p>
+            MOZR will provide service availability of 99.99% (“Service Availability”), calculated on a calendar month basis. The Service Availability will be calculated as follows:
+            [Total number of minutes Service is available in a calendar month DIVIDED BY
+            (Total number of minutes in a calendar month LESS Excused Downtime)]
+            </p>
+            <h2>2. HOW TO RECEIVE SLA CREDITS.</h2>
+            <p>
+            Customer will receive a credit of five percent (5%) of Customer’s monthly fee for each hour in which we fail to meet the Service Availability for such month (“<b>SLA Credits</b>”). 
+            In order to receive SLA Credits, Customer must make a request in writing to MOZR via Support or to Customer’s account manager (if applicable) within 30 days of the event giving rise to such SLA Credits. SLA Credits are based on our monitoring, may not exceed the total amount of recurring fees Customer has paid to us for the month in which we failed to meet the Service Availability, are forfeited at the expiration or termination of the Agreement, may not be aggregated, 
+            and will not be paid in cash.
+            </p>
+            <h2>3. EXCUSED DOWNTIME.</h2>
+            <p>
+            “<b>Excused Downtime</b>” means:
+            </p>
+            <ol>
+                <li>scheduled outages or Force Majeure events;</li>
+                <li>downtime caused by a non-standard environment, Customer machine access, Customer’s violation of the Agreement including the Acceptable Use Policy, Customer authored code or changes to the Site or Services by parties other than MOZR, or use that exceeds Customer’s plan capacity (e.g. visitors that exceed the limit for the Customer’s plan);</li>
+                <li>emergency maintenance (e.g. in order to apply a patch to address a security vulnerability); and</li>
+                <li>Scheduled maintenance that is performed during a pre-approved time by Customer</li>
+            </ol>
         </div>
     </div>
 
