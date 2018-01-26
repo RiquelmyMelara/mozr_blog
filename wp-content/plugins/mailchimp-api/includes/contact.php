d8f40d2e71e1a354f43f0c459e00db99147ea01d
Riquelmy Melara <melarr01@stepstone.com>
2018-01-26 12:03:04 -0600

    Updates to contact page

--- wp-content/plugins/mailchimp-api/includes/contact.php
+++ wp-content/plugins/mailchimp-api/includes/contact.php
@@ -24,7 +24,7 @@
                     </div>
                     <div class="col-sm-10">
                         <h2 class="contact-h2">Call Sales Now</h2>
-                        <span>1-877-973-6446</span>
+                        <span>1-801-855-8325</span>
                     </div>
                 </div>
                 <div class="col-sm-12 padding-top-35px">
@@ -33,10 +33,10 @@
                     </div>
                     <div class="col-sm-10">
                         <h2 class="contact-h2">Sales Hours</h2>
-                        <span>7am - 7pm CST</span>
+                        <span>8am - 8pm MST</span>
                         <h2 class="contact-h2">Sales Contact Info</h2>
                         <span>sales@mozr.com</span>
-                        <span>1-877-973-6446</span>
+                        <span>1-801-855-8325</span>
                     </div>
                 </div>
                 <div class="col-sm-12 padding-top-35px">
@@ -45,8 +45,8 @@
                     </div>
                     <div class="col-sm-10">
                         <h2 class="contact-h2">Mailing Address</h2>
-                        <span>Irongate house, 22-30 Duke's Place</span>
-                        <span>London, EC3A 7LP</span>
+                        <span>563 S 600 W</span>
+                        <span>Mapleton, UT 84664</span>
                     </div>
                 </div>
             </div>
@@ -54,15 +54,15 @@
         <div class="col-sm-6 contact-grey-bk-1 padding-35 margin-top-50-mobil">
             <div id="contact-form" class="col-sm-12">
             <input type='hidden' id="mailchimpcontact-listid" value="<?php echo $listId; ?>"/>
-            Full Name <input id="contact-fullname" class="contact-input" type="text" name="name"><br>
-            Email Address <input id="contact-email" class="contact-input" type="text" name="email"><br>
-            Contact us concerning* 
+            <span class="contact-span">Full Name</span> <input id="contact-fullname" class="contact-input" type="text" name="name"><br>
+            <span class="contact-span">Email Address</span> <input id="contact-email" class="contact-input" type="text" name="email"><br>
+            <span class="contact-span">Contact us concerning* </span>
             <select id="contact_type" name="contact_type" class="form-control contact-input">
                 <option value="sales">Sales</option>
                 <option value="support">Support</option>
             </select>
-            Subject* <input id="contact_sbjt" class="contact-input" type="text" name="subject"><br>
-            Message* <textarea id="contact_messg" class="contact-input" rows="6" cols="20" name="message"></textarea><br>
+            <span class="contact-span">Subject*</span> <input id="contact_sbjt" class="contact-input" type="text" name="subject"><br>
+            <span class="contact-span">Message*</span> <textarea id="contact_messg" class="contact-input" rows="6" cols="20" name="message"></textarea><br>
             <input id="contact-form-submit" type="submit" name="submit" class="contact-submit" value="Submit">
             
             <div style='display:none;margin-left: 15%;' class='loader col-md-4'>
